179e9ee7934abc23f68a212998043011c744df60
Jackson2taLL <jacksonkta10@gmail.com>
2023-10-03 18:19:15 -0700

    set up tenzies app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Die from './components/Die.jsx';
+
+function App() {
+  return (
+    <main className='main'>
+      <h1>Hello Bitches</h1>
+      <Die />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Die.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Die = () => {
+  return (
+    <div>
+      <h1>DIE</h1>
+    </div>
+  );
+};
+
+export default Die;
